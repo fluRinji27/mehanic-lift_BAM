51d1ed4a82462bccf90575c85955e83181036b6e
fluRinji27 <denisov159742@gmail.com>
2020-09-11 14:50:07 +0300

    1) Перенос состаяния модального окна в Modal.js

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,15 +1,14 @@
 const searchIcon = document.body.querySelector('.search');
 const menuButton = document.body.querySelector('.drop-menu');
-let modalState = false;
 
 
 const searchModal = new Modal(`
     <form action="/catalog/search" method="post" class="modal__search-form">
-    <input placeholder="Ведите артикул или наименование..." type="text" name="search" class="modal__input search-form__input-inner">
+    <input placeholder="Ведите артикул или наименование..." type="text" name="search" class="modal__input modal__input-inner">
     <input class="search-form__button search-form__button-inner" type="submit" value="Найти">
     </form>`);
 
-const menuModal = new Modal(`    <nav class="navigation">
+const menuModal = new Modal(`<nav class="navigation">
         <ul class="nav-list owl-carousel__footer nav-list_theme_modal">
             <li class="nav-list__item footer__inner">
                 <a href="#" class="nav-list__link">
@@ -48,22 +47,6 @@ const menuModal = new Modal(`    <nav class="navigation">
             </li>
         </ul>
     </nav>`);
-
-const createModal = body => {
-    if (!modalState) {
-        modalState = true;
-
-        body.createModal();
-
-        let modalCloseBtn = document.body.querySelector('.modal__close-btn');
-
-        modalCloseBtn.addEventListener('click', () => {
-            body.destroyModal();
-            modalState = false;
-        })
-    }
-}
-
 
 searchIcon.addEventListener('click', () => createModal(searchModal));
 menuButton.addEventListener('click', () => createModal(menuModal));
