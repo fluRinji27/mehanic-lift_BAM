17fbac2a1963988eb6100842b9b2e354d3a2ba51
fluRinji27 <denisov159742@gmail.com>
2020-09-11 14:48:42 +0300

    1) Доработка окна обратной связи 2) Обнуление анимации у нкопки обратного вызова после наведения

--- src/js/footer/footer.js
+++ src/js/footer/footer.js
@@ -1,4 +1,4 @@
-// owl-carousel
+// Анимация навигации в подвале
 $(function () {
     let owl = $(".owl-carousel__footer");
     owl.owlCarousel({
@@ -20,28 +20,29 @@ $(function () {
 // Анимация звона колокольчика в кнопке обратной связи
 let callbackButton = document.querySelector('.callback');
 let callbackImage = document.querySelector('.callback__img');
+
+// Анимация звона колокольчика
 const animateBell = () => {
     callbackImage.style.animationName = 'icon-bell';
     callbackImage.style.animationDuration = '1s';
     callbackImage.style.animationTimingFunction = 'linear';
     callbackImage.style.animationIterationCount = '1';
-    console.log('hover')
+    setTimeout(() => {
+        callbackImage.style.animation = 'none';
+    }, 1001)
+
 };
 
 callbackButton.addEventListener('mouseenter', animateBell);
 
-const searchIcon = document.body.querySelector('.search');
-const menuButton = document.body.querySelector('.drop-menu');
-let modalState = false;
-
-
 const callbackModal = new Modal(`
     <form action="/callback" method="post" class="modal__callback">
-    <input placeholder="Ваше имя.." type="text" name="name" class="modal__input">
-    <input class="search-form__button search-form__button-inner" type="submit" value="Найти">
+    <input placeholder="Ваше имя.." type="text" name="name" class="modal__input modal__input-inner">
+    <input placeholder="Ваш телефон или почта.." type="text" name="phoneOrEmail" class="modal__input modal__input-inner">
+    <textarea placeholder="Ваш вопрос.." class="modal__textarea modal__input-inner" name="message" ></textarea>
+    <input class="search-form__button search-form__button-inner" type="submit" value="Отправить">
     </form>`);
 
 
-searchIcon.addEventListener('click', () => createModal(searchModal));
-menuButton.addEventListener('click', () => createModal(menuModal));
+callbackButton.addEventListener('click', () => createModal(callbackModal));
 
