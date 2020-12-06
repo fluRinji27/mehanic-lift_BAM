3395b12a8c48a41b8febb461766af16011a8af0a
fluRinji27 <denisov159742@gmail.com>
2020-12-06 11:55:22 +0300

    6.12.20 some commit

--- config/db_params.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'host' => 'localhost',
-    'dbname' => 'u0670993_general',
-    'user' => 'root',
-    'password' => 'root',
-);
--- src/js/module/marginFooter.js
+++
@@ -1,6 +0,0 @@
-let main = document.body.getElementsByTagName('main');
-let footer = document.body.querySelector('.footer');
-if (footer) {
-    main[0].style.marginBottom = footer.clientHeight + 'px'
-
-}
